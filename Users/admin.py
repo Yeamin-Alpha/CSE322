8df978467b890875960270baa16ee3223e95efd6
Towhidul Islam Yeamin <towhidul.islam.yeamin038@gmail.com>
2025-06-01 02:32:05 +0600

    Delete Users directory

--- Users/admin.py
+++
@@ -1,9 +0,0 @@
-#Yeamin
-from django.contrib import admin
-from .models import ProfileImage, PublicProfile, Rating, SkillOption, profile,Skill
-admin.site.register(profile)
-admin.site.register(Skill)
-admin.site.register(SkillOption)
-admin.site.register(PublicProfile)
-admin.site.register(ProfileImage)
-admin.site.register(Rating)
--- Users/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import ProfileImage
-#Yeamin
-class ProfileImageForm(forms.ModelForm):
-    class Meta:
-        model = ProfileImage
-        fields = ['image']
