6dcb8a5eda8be2cd69ac09b1ba9f520e1a4c1eab
Towhidul Islam Yeamin <towhidul.islam.yeamin038@gmail.com>
2025-06-01 02:31:45 +0600

    Delete SkillBridge directory

--- SkillBridge/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def login(request):
-    return render(request,template_name='login.html')
-
-def index(request):
-    return render(request,template_name='index.html')
